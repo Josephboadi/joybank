6dae13ec5428239452bfcf62106a5a0b048ea199
Josephboadi <boadijoseph151@gmail.com>
2024-05-15 00:04:25 +0100

    add dbdocs

--- api/user.go
+++ api/user.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	db "github.com/josephboadi/joybank/db/sqlc"
 	"github.com/josephboadi/joybank/util"
 	"github.com/lib/pq"
@@ -79,11 +80,11 @@ type loginUserRequest struct {
 }
 
 type loginUserResponse struct {
-	// SessionID             uuid.UUID    `json:"session_id"`
+	SessionID             uuid.UUID    `json:"session_id"`
 	AccessToken           string       `json:"access_token"`
-	// AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
-	// RefreshToken          string       `json:"refresh_token"`
-	// RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
+	AccessTokenExpiresAt  time.Time    `json:"access_token_expires_at"`
+	RefreshToken          string       `json:"refresh_token"`
+	RefreshTokenExpiresAt time.Time    `json:"refresh_token_expires_at"`
 	User                  userResponse `json:"user"`
 }
 
@@ -118,53 +119,47 @@ func (server *Server) loginUser(ctx *gin.Context) {
 		return
 	}
 
-	accessToken, _, err := server.tokenMaker.CreateToken(
+	accessToken, accessPayload, err := server.tokenMaker.CreateToken(
 		user.Username,
 		// user.Role,
 		server.config.AccessTokenDuration,
 	)
-
-	// accessToken, accessPayload, err := server.tokenMaker.CreateToken(
-	// 	user.Username,
-	// 	// user.Role,
-	// 	server.config.AccessTokenDuration,
-	// )
 
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 
-	// refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
-	// 	user.Username,
-	// 	// user.Role,
-	// 	server.config.RefreshTokenDuration,
-	// )
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	// 	return
-	// }
+	refreshToken, refreshPayload, err := server.tokenMaker.CreateToken(
+		user.Username,
+		// user.Role,
+		server.config.RefreshTokenDuration,
+	)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
-	// session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
-	// 	ID:           refreshPayload.ID,
-	// 	Username:     user.Username,
-	// 	RefreshToken: refreshToken,
-	// 	UserAgent:    ctx.Request.UserAgent(),
-	// 	ClientIp:     ctx.ClientIP(),
-	// 	IsBlocked:    false,
-	// 	ExpiresAt:    refreshPayload.ExpiredAt,
-	// })
-	// if err != nil {
-	// 	ctx.JSON(http.StatusInternalServerError, errorResponse(err))
-	// 	return
-	// }
+	session, err := server.store.CreateSession(ctx, db.CreateSessionParams{
+		ID:           refreshPayload.ID,
+		Username:     user.Username,
+		RefreshToken: refreshToken,
+		UserAgent:    ctx.Request.UserAgent(),
+		ClientIp:     ctx.ClientIP(),
+		IsBlocked:    false,
+		ExpiresAt:    refreshPayload.ExpiredAt,
+	})
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
 
 	rsp := loginUserResponse{
-		// SessionID:             session.ID,
+		SessionID:             session.ID,
 		AccessToken:           accessToken,
-		// AccessTokenExpiresAt:  accessPayload.ExpiredAt,
-		// RefreshToken:          refreshToken,
-		// RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
+		AccessTokenExpiresAt:  accessPayload.ExpiredAt,
+		RefreshToken:          refreshToken,
+		RefreshTokenExpiresAt: refreshPayload.ExpiredAt,
 		User:                  newUserResponse(user),
 	}
 	ctx.JSON(http.StatusOK, rsp)
